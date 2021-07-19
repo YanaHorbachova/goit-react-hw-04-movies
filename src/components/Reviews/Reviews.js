f9d8d8b4c04ab5d1354ac8df4067d77ac9301ada
YanaHorbachova <maya69877@gmail.com>
2021-07-19 14:35:33 +0300

    стили Reviews

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import style from './Reviews.module.css'
 
 const Reviews = ({ reviews }) => {
     return (
       <>
         {reviews.length > 0 ? (
-          <ul>
+          <ul className={style.List}>
             {reviews.map(review => (
-              <li key={review.id}>
+              <li className={style.ListItem} key={review.id}>
                 <h3>{review.author}</h3>
                 <p>{review.content}</p>
               </li>
             ))}
           </ul>
         ) : (
-          <h2>
+          <h2 className={style.NoReviewsTitle}>
             We don't have any reviews to this movie
           </h2>
         )}
